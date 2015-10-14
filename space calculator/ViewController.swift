3827d9a375b7d4f9acddaeade71d85212da245b6
Hugo Prinsloo <8c8a8t8@gmail.com>
2015-10-14 18:48:27 +0200

    Initial Commit

---
+++ space calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  space calculator
+//
+//  Created by Hugo Prinsloo on 2015/10/14.
+//  Copyright © 2015 Hugo Prinsloo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
